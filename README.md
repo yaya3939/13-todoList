c34978915d8e8af7e40572bad7ce3e4554df1574
yaya3939 <1220593514@qq.com>
2022-10-26 21:49:15 +0800

    complete 2nd version todolist. mongoose, route params.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-2022/10/20
-
 ## 学了啥
+
+### 2022/10/20
 
 - ejs: template,layouts
 - scope
@@ -10,7 +10,18 @@
 - 重温 funcion()。括号里的变量的使用，加括号是要调动 function。
 - 重温 if/else,switch,for
 
-## 改进点
+### 10/26
 
-- check 了 item 之后，再加新的 item 会整个刷新，之前 checked item 会变成没有 check 的状态
+- string 引号里面不要乱加空格，会直接导致 value 变不一样。checkbox 的 value 不小心加了一个空格，导致我要删除的 item 的 id 不对了
+- 怎么用 mongoose 把 database 用起来
+- 复习了 forEach。尽量选择最贴合本质逻辑的方式去编写代码。
+- 复习了用 route params 来自己创造新网页
+- 编写 ejs 文件的时候最好把 format on save 关掉，要不然真的经常出现 syntax error
+
+## 问题
+
+- √ ：check 了 item 之后，再加新的 item 会整个刷新，之前 checked item 会变成没有 check 的状态。
+  solution：mongoose database
+- √ ：总是出现 SyntaxError: Unexpected identifier in C:\Users\lenovo\Desktop\webDevelop\13-todoList-v1-ejs\views\list.ejs while compiling ejs。
+  solution：观察了很久代码，发现有几个普通的 html 的 tag 的关闭部分都很奇怪，是红色的。所以把 1st version 的代码 copy 过来，替换了，但是保存以后又会变得很奇怪，关掉 format on save 就好了。
 - 喜欢的外观
--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 /*利用node的module概念，将所有不在逻辑线条内的代码块
 移动到别的文件去，可以让逻辑和代码看起来更清晰简洁易懂*/
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,49 +14,128 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 //datas
-const items = ["Buy food", "Read book", "Text preparation"];
-const workItems = [];
+//调用其他module下的function的具体方式：
+const day = date.getDate("en-US");
+//en-US, zh-CN...
+
+//没有数据库时候的datas
+// const items = ["Buy food", "Read book", "Text preparation"];
+// const workItems = [];
 //在开头设置空的变量，避免后面因为顺序问题导致的not defined error
 //要连续加新的item，所以选择array。这样就不会出现新的item directly replace old one的问题
 //把array、object设置成const，依然可以改变增减里面的项目。只是不能整个重新设定
 
-app.get("/", function (req, res) {
-  //调用其他module下的function的具体方式：
-  const day = date.getDate("en-US");
-  //en-US, zh-CN...
+//database
+mongoose.connect("mongodb://localhost:27017/todolistDB");
 
-  res.render("list", { listTitle: day, newListItems: items });
-  /*渲染一个叫list的文件，给其中名为listTitle的key，赋予一个day的value
+// item collection
+const itemSchema = new mongoose.Schema({ name: String });
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({ name: "Welcome to your todolist!" });
+const item2 = new Item({ name: "Hit the + button to add a new item." });
+const item3 = new Item({ name: "<-- Hit this to delete an item." });
+const defaultItems = [item1, item2, item3];
+
+//list collection
+const listSchema = new mongoose.Schema({
+  name: String,
+  items: [itemSchema],
+});
+const List = mongoose.model("List", listSchema);
+
+app.get("/", function (req, res) {
+  Item.find({}, function (err, foundItems) {
+    if (foundItems.length === 0) {
+      //怎么检查array里的项的数量
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("successfully insert default items.");
+        }
+      });
+      res.redirect("/"); //这样才能显示，要不然就没有render了
+    } else {
+      res.render("list", { listTitle: day, newListItems: foundItems });
+      /*渲染一个叫list的文件，给其中名为listTitle的key，赋予一个day的value
   the key of object have to exist in ejs file
   把key放在一个render里，可以避免因某些key暂时没有值而崩溃*/
-});
-
-app.get("/work", function (req, res) {
-  res.render("list", { listTitle: "WORK LIST", newListItems: workItems });
+    }
+  });
 });
 
 app.get("/about", function (req, res) {
   res.render("about");
 });
 
+//customi lists by route params
+app.get("/:custom", function (req, res) {
+  const customList = _.capitalize(req.params.custom);
+
+  List.findOne({ name: customList }, function (err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        //!:dont't have sth
+        //create new list
+        const list = new List({
+          name: customList,
+          items: defaultItems,
+        });
+        list.save();
+        res.redirect("/" + customList);
+      } else {
+        //show an existing list
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items,
+        });
+      }
+    }
+  });
+});
+
 app.post("/", function (req, res) {
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
 
-  if (req.body.list === "WORK") {
-    //不要自己臆想list的值，一定要用consloe.log确定
+  //把数据传到数据库
+  const item = new Item({ name: itemName });
 
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
+  if (listName === day) {
+    item.save();
     res.redirect("/");
-    /*when post was called, 先把数据记录到server里，然后redirect to
-    root route,再次激活app.get("/"...)*/
+  } else {
+    List.findOne({ name: listName }, function (err, foundList) {
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
   }
+});
 
-  /*res.render("list", { newListItem: item }); :
-  it wouldn't work. because when u run the local root 
-  at the beginning, there's no value of it*/
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === day) {
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+      if (!err) {
+        console.log("successfully delete checked item");
+        res.redirect("/");
+      }
+    });
+  } else {
+    List.findOneAndUpdate(
+      { name: listName },
+      { $pull: { items: { _id: checkedItemId } } },
+      function (err, foundList) {
+        if (!err) {
+          res.redirect("/" + listName);
+        }
+      }
+    );
+  }
 });
 
 app.listen(3000, function () {
